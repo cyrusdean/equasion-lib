cc2389a84ee80074bcbb917049e259a77fb1aa02
Cyrus Dean <cyrusdean@Cyruss-Mac-mini.local>
2022-06-14 16:18:59 -0700

    Test

--- src/forms/Select/com/AppSelect/AppSelect.tsx
+++ src/forms/Select/com/AppSelect/AppSelect.tsx
@@ -117,7 +117,6 @@ const AppSelect = ({
                   )
                     updateValue('')
                 }
-                setPopoverOpen(false)
               }}
               onFocus={() => {
                 setFilterValue('')
