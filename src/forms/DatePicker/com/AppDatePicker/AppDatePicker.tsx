a049172c4305e69a19b339f3eb6b8ca8929a6d28
Cyrus Dean <cyrusdean@Cyruss-Mac-mini.local>
2022-07-12 08:18:37 -0700

    Fixed Datepicker popover

--- src/forms/DatePicker/com/AppDatePicker/AppDatePicker.tsx
+++ src/forms/DatePicker/com/AppDatePicker/AppDatePicker.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import { Calendar } from 'react-date-range'
+import { Popover } from '../../../../elements'
 
 const AppDatePicker = ({
   label,
@@ -9,17 +10,8 @@ const AppDatePicker = ({
   iconAfter,
   ...rest
 }) => {
-  const inputRef = useRef(null)
+  const [popoverOpen, setPopoverOpen] = useState(false)
   const [currentValue, setCurrentValue] = useState(defaultValue)
-  const [pickerOpen, setPickerOpen] = useState(false)
-  const [valueSelected, setValueSelected] = useState(false)
-
-  const checkFocusOfInput = () => {
-    setTimeout(() => {
-      const inputEl = inputRef.current
-      if (inputEl != document.activeElement) setPickerOpen(false)
-    }, 150)
-  }
 
   const updateValue = (value) => {
     setCurrentValue(value)
@@ -30,45 +22,46 @@ const AppDatePicker = ({
     'eq-datepicker',
     iconBefore ? 'icon-before' : '',
     iconAfter ? 'icon-after' : '',
-    pickerOpen ? 'open' : '',
   ].join(' ')
 
   return (
     <div className={combinedClasses}>
-      <span className="eq-input-icon">{iconBefore}</span>
-      <input
-        ref={inputRef}
-        {...rest}
-        onChange={() => {}}
-        onFocus={() => {
-          if (!valueSelected) setPickerOpen(true)
-          else setValueSelected(false)
-        }}
-        onClick={(e) => {
-          e.preventDefault()
-          setPickerOpen(true)
-        }}
-        onBlur={checkFocusOfInput}
-        value={new Date(currentValue).toISOString().split('T')[0]}
-        placeholder=" "
-        spellCheck={false}
-        type="date"
-      />
-      <div onClick={() => inputRef.current.focus()}>
-        <Calendar
-          color="var(--primaryColor)"
-          className={pickerOpen ? 'open' : ''}
-          date={new Date(currentValue)}
-          onChange={(newDate) => {
-            const newDateFormatted = new Date(newDate).toLocaleDateString()
-            updateValue(newDateFormatted)
-            setValueSelected(true)
-            setPickerOpen(false)
-          }}
-        />
-      </div>
-      <span className="eq-input-icon">{iconAfter}</span>
-      {!!label && <label>{label}</label>}
+      <Popover
+        className="eq-datepicker-popover"
+        position="bottom"
+        align="start"
+        // @ts-ignore
+        padding={1}
+        isPopoverOpen={popoverOpen}
+        setIsPopoverOpen={(open) => setPopoverOpen(open)}
+        content={
+          <div>
+            <Calendar
+              color="var(--primaryColor)"
+              date={new Date(currentValue)}
+              onChange={(newDate) => {
+                const newDateFormatted = new Date(newDate).toLocaleDateString()
+                updateValue(newDateFormatted)
+                setPopoverOpen(false)
+              }}
+            />
+          </div>
+        }
+      >
+        <>
+          <span className="eq-input-icon">{iconBefore}</span>
+          <input
+            {...rest}
+            onChange={() => {}}
+            value={new Date(currentValue).toISOString().split('T')[0]}
+            placeholder=" "
+            spellCheck={false}
+            type="date"
+          />
+          <span className="eq-input-icon">{iconAfter}</span>
+          {!!label && <label>{label}</label>}
+        </>
+      </Popover>
     </div>
   )
 }
