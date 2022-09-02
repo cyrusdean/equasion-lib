198cadb61f69e253eeec4e5e58e08a1a6233b657
Jake Madsen <jake@equasion.com>
2022-09-02 12:21:30 -0600

    mo changes

--- src/elements/Modal/Modal.tsx
+++ src/elements/Modal/Modal.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { IoClose } from 'react-icons/io5';
-import Modal from 'react-modal';
-import './Modal.scss';
+import React from 'react'
+import { FiX } from 'react-icons/fi'
+import Modal from 'react-modal'
+import './Modal.scss'
 
-Modal.setAppElement('#app');
+Modal.setAppElement('#app')
 
 const ModalWrap = ({ modalOpen, setModalOpen, children, className }) => {
-  const combinedClasslassName = ['react-modal', className].join(' ');
+  const combinedClasslassName = ['react-modal', className].join(' ')
   return (
     <Modal
       isOpen={modalOpen}
@@ -14,12 +14,12 @@ const ModalWrap = ({ modalOpen, setModalOpen, children, className }) => {
       shouldCloseOnOverlayClick
       className={combinedClasslassName}
       overlayClassName="react-modal-overlay"
-      closeTimeoutMS={400}
+      closeTimeoutMS={0}
     >
-      <IoClose onClick={() => setModalOpen(false)} />
+      <FiX onClick={() => setModalOpen(false)} />
       {children}
     </Modal>
-  );
-};
+  )
+}
 
-export default ModalWrap;
+export default ModalWrap
